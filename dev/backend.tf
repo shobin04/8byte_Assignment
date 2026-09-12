7d254bf1b11e7b05610e274dd1f8f0c34c0a063d
shobin04 <104362015+shobin04@users.noreply.github.com>
2026-09-12 18:20:32 +0530

    Delete dev directory

--- dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraformbucketstate-shobin-8byte-2026"
-    key          = "dev/terraform.tfstate"
-    region       = "us-east-1"
-    dynamodb_table = "terraform-state-locks"
-    encrypt      = true
-  }
-}
