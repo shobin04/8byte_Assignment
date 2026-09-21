b12ecf9898a67074efcb00cdd5d058c980f70c85
Shobin <percyshobin@gmail.com>
2026-09-21 18:49:30 +0000

    terraform

--- terraform_aws/modules/bastion_security_group/main.tf
+++ terraform_aws/modules/bastion_security_group/main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = "22"
     to_port     = "22"
     protocol    = "tcp"
-    cidr_blocks = ["223.181.12.56/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
